ffc9efd3556b979ccb16be994adf6c6ac40dbebe
mo <muhammadelkhateb95@gmail.com>
2020-08-20 17:52:54 +0200

    DB Migrations & Seeders

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiresource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+});
