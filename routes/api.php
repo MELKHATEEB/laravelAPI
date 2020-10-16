c3fc00305083b699876bbec0afd63befbd8dc02b
mo <muhammadelkhateb95@gmail.com>
2020-10-16 14:06:36 +0200

    Update Product

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiresource('/products', 'ProductController');
+Route::apiResource('/products', 'ProductController');
 
 Route::group(['prefix' => 'products'], function () {
     Route::apiResource('/{product}/reviews', 'ReviewController');
