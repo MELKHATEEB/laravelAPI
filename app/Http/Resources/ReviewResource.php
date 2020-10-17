3f61868b027590fe7e969d6d3977334802833121
mo <muhammadelkhateb95@gmail.com>
2020-10-17 11:36:23 +0200

    Update & Delete Reviews

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -16,6 +16,7 @@ class ReviewResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
+            'id' => $this->id,
             'customer' => $this->customer,
             'body' => $this->review,
             'star' => $this->star
